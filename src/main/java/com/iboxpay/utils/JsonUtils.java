73e2de2e27471e6b4d7d97715dedadf483761023
liuyalou <liuyalou@iboxpay.com>
2018-07-31 15:49:45 +0800

    修改格式

--- src/main/java/com/iboxpay/utils/JsonUtils.java
+++ src/main/java/com/iboxpay/utils/JsonUtils.java
@@ -12,118 +12,118 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class JsonUtils {
 
-	/**
-	 * 将对象转换为json字符串
-	 * 
-	 * @param obj
-	 *            转换的对象
-	 * @return
-	 */
-	public static String toJsonString(Object obj) {
-		return toJsonString(obj, false);
-	}
+  /**
+   * 将对象转换为json字符串
+   * 
+   * @param obj
+   *            转换的对象
+   * @return
+   */
+  public static String toJsonString(Object obj) {
+    return toJsonString(obj, false);
+  }
 
-	/**
-	 * 对象转换为json字符串，可允许转换空值
-	 * 
-	 * @param obj
-	 *            转换的对象
-	 * @param isNullValueAllowed
-	 *            是否允许空值
-	 * @return
-	 */
-	public static String toJsonString(Object obj, boolean isNullValueAllowed) {
-		if (obj == null) {
-			return null;
-		}
+  /**
+   * 对象转换为json字符串，可允许转换空值
+   * 
+   * @param obj
+   *            转换的对象
+   * @param isNullValueAllowed
+   *            是否允许空值
+   * @return
+   */
+  public static String toJsonString(Object obj, boolean isNullValueAllowed) {
+    if (obj == null) {
+      return null;
+    }
 
-		if (!isNullValueAllowed) {
-			return JSON.toJSONString(obj);
-		}
+    if (!isNullValueAllowed) {
+      return JSON.toJSONString(obj);
+    }
 
-		return JSON.toJSONString(obj, SerializerFeature.WRITE_MAP_NULL_FEATURES);
-	}
+    return JSON.toJSONString(obj, SerializerFeature.WRITE_MAP_NULL_FEATURES);
+  }
 
-	/**
-	 * 将json字符串转换为javaBean
-	 * 
-	 * @param jsonStr
-	 *            json字符串
-	 * @param clazz
-	 *            运行时对象
-	 * @return
-	 */
-	public static <T> T toJavaBean(String jsonStr, Class<T> clazz) {
-		if (StringUtils.isBlank(jsonStr)) {
-			return null;
-		}
+  /**
+   * 将json字符串转换为javaBean
+   * 
+   * @param jsonStr
+   *            json字符串
+   * @param clazz
+   *            运行时对象
+   * @return
+   */
+  public static <T> T toJavaBean(String jsonStr, Class<T> clazz) {
+    if (StringUtils.isBlank(jsonStr)) {
+      return null;
+    }
 
-		return JSON.parseObject(jsonStr, clazz);
-	}
+    return JSON.parseObject(jsonStr, clazz);
+  }
 
-	/**
-	 * 字符串转换为list
-	 * 
-	 * @param jsonStr
-	 *            json字符串
-	 * @param clazz
-	 *            运行时对象
-	 * @return
-	 */
-	public static <T> List<T> toList(String jsonStr, Class<T> clazz) {
-		if (StringUtils.isBlank(jsonStr)) {
-			return null;
-		}
+  /**
+   * 字符串转换为list
+   * 
+   * @param jsonStr
+   *            json字符串
+   * @param clazz
+   *            运行时对象
+   * @return
+   */
+  public static <T> List<T> toList(String jsonStr, Class<T> clazz) {
+    if (StringUtils.isBlank(jsonStr)) {
+      return null;
+    }
 
-		return JSON.parseArray(jsonStr, clazz);
-	}
+    return JSON.parseArray(jsonStr, clazz);
+  }
 
-	/**
-	 * 将json字符串转换为map
-	 * 
-	 * @param jsonStr
-	 *            json字符串
-	 * @return
-	 */
-	public static Map<String, Object> toMap(String jsonStr) {
-		if (StringUtils.isBlank(jsonStr)) {
-			return null;
-		}
+  /**
+   * 将json字符串转换为map
+   * 
+   * @param jsonStr
+   *            json字符串
+   * @return
+   */
+  public static Map<String, Object> toMap(String jsonStr) {
+    if (StringUtils.isBlank(jsonStr)) {
+      return null;
+    }
 
-		return JSON.parseObject(jsonStr, new TypeReference<Map<String, Object>>() {});
-	}
+    return JSON.parseObject(jsonStr, new TypeReference<Map<String, Object>>() {});
+  }
 
-	/**
-	 * 将javaBean转换为map
-	 * 
-	 * @param obj
-	 *            转换的对象
-	 * @return
-	 */
-	public static Map<String, Object> javaBeanToMap(Object obj) {
-		if (obj == null) {
-			return null;
-		}
+  /**
+   * 将javaBean转换为map
+   * 
+   * @param obj
+   *            转换的对象
+   * @return
+   */
+  public static Map<String, Object> javaBeanToMap(Object obj) {
+    if (obj == null) {
+      return null;
+    }
 
-		return toMap(toJsonString(obj));
-	}
+    return toMap(toJsonString(obj));
+  }
 
-	/**
-	 * 将map转换为javaBean
-	 * 
-	 * @param map
-	 *            map实例
-	 * @param clazz
-	 *            运行时对象
-	 * @return
-	 */
-	public static <T> T mapToJavaBean(Map<String, ? extends Object> map, Class<T> clazz) {
-		if (CollectionUtils.isEmpty(map)) {
-			return null;
-		}
+  /**
+   * 将map转换为javaBean
+   * 
+   * @param map
+   *            map实例
+   * @param clazz
+   *            运行时对象
+   * @return
+   */
+  public static <T> T mapToJavaBean(Map<String, ? extends Object> map, Class<T> clazz) {
+    if (CollectionUtils.isEmpty(map)) {
+      return null;
+    }
 
-		String jsonStr = JSON.toJSONString(map);
-		return JSON.parseObject(jsonStr, clazz);
-	}
+    String jsonStr = JSON.toJSONString(map);
+    return JSON.parseObject(jsonStr, clazz);
+  }
 
 }
